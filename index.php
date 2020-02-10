4b6b0cde3bd0f8670f83000a6df1a13cdfdef18c
Alex Jenkinson <alex.jenkinson@verdite.co.uk>
2020-02-10 19:15:30 +0000

    Removed some default tags

--- index.php
+++ index.php
@@ -16,17 +16,6 @@ require_once($_SERVER['DOCUMENT_ROOT'] . DOCROOT . "includes/head.php");
     <?php require_once($_SERVER['DOCUMENT_ROOT'] . DOCROOT . "includes/navbar.php "); ?>
     <!-- / Navbar -->
 
-    <!-- <section class="section">
-        <div class="container">
-            <h1 class="title">
-                Hello World
-            </h1>
-            <p class="subtitle">
-                My first website with <strong>Bulma</strong>!
-            </p>
-        </div>
-    </section> -->
-
     <section class="section">
 
         <div class="columns">
