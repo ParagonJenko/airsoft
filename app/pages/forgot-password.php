bffab2c5cb1b6c2ca17de772be769743f09724a2
Alex Jenkinson <alex.jenkinson@devel-ops.com>
2020-02-19 01:29:28 +0000

    Added other user pages ready for forms

---
+++ app/pages/forgot-password.php
@@ -0,0 +1,41 @@
+<!-- Init -->
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/airsoft/env.php "); ?>
+<!-- / Init -->
+
+<!DOCTYPE html>
+<html>
+<!-- Head -->
+<?php
+$title = "Forgot Password";
+require_once($_SERVER['DOCUMENT_ROOT'] . DOCROOT . "includes/head.php");
+?>
+<!-- / Head -->
+
+<body>
+    <!-- Navbar -->
+    <?php require_once($_SERVER['DOCUMENT_ROOT'] . DOCROOT . "includes/navbar.php "); ?>
+    <!-- / Navbar -->
+
+    <section class="hero is-fullheight ">
+        <div class="hero-body">
+            <div class="container">
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . DOCROOT . "includes/footer.php");
+    ?>
+    <!-- / Footer -->
+
+</body>
+
+<!-- Scripts -->
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . DOCROOT . "includes/scripts.php");
+?>
+<!-- / Scripts -->
+
+</html>
